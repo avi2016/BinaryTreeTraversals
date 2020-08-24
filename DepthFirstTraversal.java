e3c7512f6f21f21fe93e733e16b5d7b3380fcee3
avi054 <aviralagarwal.2013@gmail.com>
2020-08-24 09:30:59 +0530

    Update DepthFirstTraversal.java

--- DepthFirstTraversal.java
+++ DepthFirstTraversal.java
@@ -1,17 +1,27 @@
 class DepthFirstTraversal {
 	public static void main (String[] args) {
 		
-		Node root = new Node(3);
+	Node root = new Node(3);
 
-		root.left = new Node(6);
-		root.left.left = new Node(2);
-		root.left.right = new Node(11);
-		root.left.right.left = new Node(9);
-		root.left.right.right = new Node(5);
+	root.left = new Node(6);
+	root.left.left = new Node(2);
+	root.left.right = new Node(11);
+	root.left.right.left = new Node(9);
+	root.left.right.right = new Node(5);
 
-		root.right = new Node(8);
+	root.right = new Node(8);
         root.right.right = new Node(13);
         root.right.right.left = new Node(7);
+		
+	/*
+                    3
+                  /  \
+                6     8
+               / \     \
+             2    11    13
+                 / \    /
+                9   5  7
+       */	
         
         BinaryTree bt = new BinaryTree();
         bt.preOrderTraversal(root);
